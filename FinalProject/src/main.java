78a66d79e188d640b243240ac64606099644de46
Sam Cardin <blucardin@gmail.com>
2022-05-17 03:26:24 +0000

    Figuered out how to run code !

---
+++ FinalProject/src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    //throws Exception
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("Hello World!");
+        System.out.println("And welcome to Noah's Games Emporium!");
+        System.out.println("We have a variety of games for you to play!");
+
+        Thread.sleep(2000);
+
+
+
+    }
+}
